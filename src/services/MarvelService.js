7a6ce9be33dde24c09a15799723bbf55a7e2161e
AndriyFursa <andriyfursa_12@gmail.com>
2022-12-11 23:02:34 +0200

    marvel_spinner_error_add

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,33 +1,43 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+  // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
   _apiKey = 'apikey=d6d8b878a548fca9201e2f928e9c7870'
+
   getResource = async (url) => {
     let res = await fetch(url)
+
     if (!res.ok) {
-      throw new Error(`Coud not fetch ${url}, status ${res.status}`)
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
     }
+
     return await res.json()
   }
+
   getAllCharacters = async () => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     )
-    return res.data.result.map(this._transformCharacter)
+    return res.data.results.map(this._transformCharacter)
   }
+
   getCharacter = async (id) => {
     const res = await this.getResource(
       `${this._apiBase}characters/${id}?${this._apiKey}`
     )
     return this._transformCharacter(res.data.results[0])
   }
+
   _transformCharacter = (char) => {
     return {
       name: char.name,
-      description: char.description,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : 'There is no description for this character',
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
     }
   }
 }
+
 export default MarvelService
