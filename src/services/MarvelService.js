ea2b249eae224d1827a227c430112c1e29932cfe
AndriyFursa <andriyfursa_12@gmail.com>
2022-12-16 23:05:36 +0200

    marvel_charinfo_errorboundary_add

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -29,6 +29,7 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description
         ? `${char.description.slice(0, 210)}...`
@@ -36,6 +37,7 @@ class MarvelService {
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
+      comics: char.comics.items,
     }
   }
 }
